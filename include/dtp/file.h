000ce6e124469958c3fbf6c18cd1e8a88af300ab
Ivan Zhilenkov <i.zhilenkov@module.ru>
2023-08-08 16:22:45 +0300

    123++

--- include/dtp/file.h
+++ include/dtp/file.h
@@ -1,6 +1,14 @@
 #ifndef __DTP_FILE_H_INCLUDED__
 #define __DTP_FILE_H_INCLUDED__
 
-int dtpOpenFile(const char *filename, const char *mode);
+#ifdef __cplusplus
+extern "C"{
+#endif //__cplusplus
+
+    int dtpOpenFile(const char *filename, const char *mode);
+
+#ifdef __cplusplus
+}
+#endif //__cplusplus
 
 #endif //__DTP_FILE_H_INCLUDED__
